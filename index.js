0a62156dd3f82ed7f58f523f3a7f89b2e8576f85
Anton Solodovnykov <solodovnikov.toni@gmail.com>
2022-11-03 18:40:17 +0200

    Connected DB

--- index.js
+++ index.js
@@ -1,32 +1,28 @@
 import express from "express";
 import cors from "cors";
 import jwt from "jsonwebtoken";
+import argon2 from "argon2";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+mongoose
+  .connect(
+    `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.7nqxsud.mongodb.net/?retryWrites=true&w=majority`
+  )
+
+  .then(() => {
+    console.log("DB is OK");
+  })
+  .catch((error) => console.log("Db Error", error));
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
 
-app.get("/", (req, res) => {
-  res.send("Hello, world!");
-});
-
-app.post("/auth/login", (req, res) => {
-  const token = jwt.sign(
-    {
-      email: req.body.email,
-      fullname: "Anton",
-    },
-    "secretkey"
-  );
-
-  res.json({
-    success: true,
-    token
-  });
-});
-
-app.listen(5555, (error) => {
+app.listen(process.env.PORT || 5555, (error) => {
   if (error) {
     return console.log(error);
   }
