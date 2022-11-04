35066072000600c5149cd5ef7f73f9522cad4771
Anton Solodovnykov <solodovnikov.toni@gmail.com>
2022-11-04 17:34:31 +0200

    Created CRUD Post Controllers

---
+++ controllers/PostController.js
@@ -0,0 +1,139 @@
+import PostModel from "../models/Post.js";
+
+export const getAll = async (req, res) => {
+  try {
+    const posts = await PostModel.find().populate("user").exec();
+
+    res.json(posts);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      message: "Failed to get posts",
+    });
+  }
+};
+
+export const getOne = async (req, res) => {
+  try {
+    const postId = req.params.id;
+
+    PostModel.findOneAndUpdate(
+      {
+        _id: postId,
+      },
+      {
+        $inc: {
+          viewsCount: 1,
+        },
+      },
+      {
+        returnDocument: "after",
+      },
+      (error, doc) => {
+        if (error) {
+          console.log(error);
+          return res.status(500).json({
+            message: "Failed to return post",
+          });
+        }
+
+        if (!doc) {
+          return res.status(404).json({
+            message: "Post not found",
+          });
+        }
+
+        res.json(doc);
+      }
+    );
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      message: "Failed to get posts",
+    });
+  }
+};
+
+export const create = async (req, res) => {
+  try {
+    const doc = new PostModel({
+      title: req.body.title,
+      text: req.body.text,
+      tags: req.body.tags,
+      imageUrl: req.body.imageUrl,
+      user: req.userId,
+    });
+
+    const post = await doc.save();
+
+    res.json(post);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      message: "Failed to create post",
+    });
+  }
+};
+
+export const remove = async (req, res) => {
+  try {
+    const postId = req.params.id;
+
+    PostModel.findOneAndRemove(
+      {
+        _id: postId,
+      },
+      (error, doc) => {
+        if (error) {
+          console.log(error);
+          return res.status(500).json({
+            message: "Failed to remove post",
+          });
+        }
+
+        if (!doc) {
+          return res.status(404).json({
+            message: "Post not found",
+          });
+        }
+
+        res.json({
+          success: true,
+        });
+      }
+    );
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      message: "Failed to get posts",
+    });
+  }
+};
+
+export const update = async (req, res) => {
+  try {
+    const postId = req.params.id;
+
+    await PostModel.updateOne(
+      {
+        _id: postId,
+      },
+      {
+        title: req.body.title,
+        text: req.body.text,
+        imageUrl: req.body.imageUrl,
+        user: req.userId,
+        tags: req.body.tags,
+      }
+    );
+
+    res.json({
+      success: true,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      message: "Failed to update post",
+    });
+  }
+};
--- index.js
+++ index.js
@@ -10,6 +10,7 @@ import {
 
 import checkAuth from "./utils/checkAuth.js";
 import * as UserController from "./controllers/UserController.js";
+import * as PostController from "./controllers/PostController.js";
 
 dotenv.config();
 
@@ -31,6 +32,12 @@ app.use(cors());
 app.post("/auth/login", loginValidation, UserController.login);
 app.post("/auth/register", registerValidation, UserController.register);
 app.get("/auth/me", checkAuth, UserController.getMe);
+
+app.get("/posts", PostController.getAll);
+app.get("/posts/:id", PostController.getOne);
+app.post("/posts", checkAuth, postCreateValidation, PostController.create);
+app.delete("/posts/:id", checkAuth, PostController.remove);
+app.patch("/posts/:id", checkAuth, PostController.update);
 
 app.listen(process.env.PORT || 5555, (error) => {
   if (error) {
