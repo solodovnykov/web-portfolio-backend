625fb15b359b59aaa173c7ee5be32a062086fe4f
Anton Solodovnykov <solodovnikov.toni@gmail.com>
2022-11-04 23:23:56 +0200

    Added EsLint

--- index.js
+++ index.js
@@ -1,88 +1,90 @@
-import express from "express";
-import cors from "cors";
-import mongoose from "mongoose";
-import dotenv from "dotenv";
-import multer from "multer";
+import express from 'express'
+import cors from 'cors'
+import mongoose from 'mongoose'
+import dotenv from 'dotenv'
+import multer from 'multer'
+import helmet from 'helmet'
 import {
-  registerValidation,
-  loginValidation,
-  postCreateValidation,
-} from "./validations.js";
+    registerValidation,
+    loginValidation,
+    postCreateValidation,
+} from './validations.js'
 
-import checkAuth from "./utils/checkAuth.js";
-import * as UserController from "./controllers/UserController.js";
-import * as PostController from "./controllers/PostController.js";
-import handleValidationErrors from "./utils/handleValidationErrors.js";
+import checkAuth from './utils/checkAuth.js'
+import * as UserController from './controllers/UserController.js'
+import * as PostController from './controllers/PostController.js'
+import handleValidationErrors from './utils/handleValidationErrors.js'
 
-dotenv.config();
+dotenv.config()
 
 mongoose
-  .connect(
-    `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.7nqxsud.mongodb.net/web-portfolio?retryWrites=true&w=majority`
-  )
+    .connect(
+        `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.7nqxsud.mongodb.net/web-portfolio?retryWrites=true&w=majority`
+    )
 
-  .then(() => {
-    console.log("DB is OK");
-  })
-  .catch((error) => console.log("Db Error", error));
+    .then(() => {
+        console.log('DB is OK')
+    })
+    .catch((error) => console.log('Db Error', error))
 
-const app = express();
+const app = express()
 
 const storage = multer.diskStorage({
-  destination: (_, __, cb) => {
-    cb(null, "uploads");
-  },
-  filename: (_, file, cb) => {
-    cb(null, file.originalname);
-  },
-});
+    destination: (_, __, cb) => {
+        cb(null, 'uploads')
+    },
+    filename: (_, file, cb) => {
+        cb(null, file.originalname)
+    },
+})
 
-const upload = multer({ storage });
+const upload = multer({ storage })
 
-app.use(express.json());
-app.use(cors());
-app.use("/uploads", express.static("uploads"));
+app.use(express.json())
+app.use(cors())
+app.use(helmet())
+app.use('/uploads', express.static('uploads'))
 
-app.post("/upload", checkAuth, upload.single("image"), (req, res) => {
-  res.json({
-    url: `/uploads/${req.file.originalname}`,
-  });
-});
+app.post('/upload', checkAuth, upload.single('image'), (req, res) => {
+    res.json({
+        url: `/uploads/${req.file.originalname}`,
+    })
+})
 
 app.post(
-  "/auth/login",
-  loginValidation,
-  handleValidationErrors,
-  UserController.login
-);
+    '/auth/login',
+    loginValidation,
+    handleValidationErrors,
+    UserController.login
+)
 app.post(
-  "/auth/register",
-  registerValidation,
-  handleValidationErrors,
-  UserController.register
-);
-app.get("/auth/me", checkAuth, UserController.getMe);
+    '/auth/register',
+    registerValidation,
+    handleValidationErrors,
+    UserController.register
+)
+app.get('/auth/me', checkAuth, UserController.getMe)
 
-app.get("/posts", PostController.getAll);
-app.get("/posts/:id", PostController.getOne);
+app.get('/posts', PostController.getAll)
+app.get('/posts/:id', PostController.getOne)
 app.post(
-  "/posts",
-  checkAuth,
-  postCreateValidation,
-  handleValidationErrors,
-  PostController.create
-);
-app.delete("/posts/:id", checkAuth, PostController.remove);
+    '/posts',
+    checkAuth,
+    postCreateValidation,
+    handleValidationErrors,
+    PostController.create
+)
+app.delete('/posts/:id', checkAuth, PostController.remove)
 app.patch(
-  "/posts/:id",
-  checkAuth,
-  handleValidationErrors,
-  PostController.update
-);
+    '/posts/:id',
+    checkAuth,
+    handleValidationErrors,
+    PostController.update
+)
 
 app.listen(process.env.PORT || 5555, (error) => {
-  if (error) {
-    return console.log(error);
-  }
-  console.log("Server has been started.");
-});
+    if (error) {
+        return console.log(error)
+    }
+    console.log('Server has been started.')
+})
--- models/Post.js
+++ models/Post.js
@@ -1,33 +1,33 @@
-import mongoose from "mongoose";
+import mongoose from 'mongoose'
 
 const PostSchema = new mongoose.Schema(
-  {
-    title: {
-      type: String,
-      required: true,
+    {
+        title: {
+            type: String,
+            required: true,
+        },
+        text: {
+            type: String,
+            required: true,
+        },
+        tags: {
+            type: Array,
+            default: [],
+        },
+        viewsCount: {
+            type: Number,
+            default: 0,
+        },
+        user: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'User',
+            required: true,
+        },
+        imageUrl: String,
     },
-    text: {
-      type: String,
-      required: true,
-    },
-    tags: {
-      type: Array,
-      default: [],
-    },
-    viewsCount: {
-      type: Number,
-      default: 0,
-    },
-    user: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "User",
-      required: true,
-    },
-    imageUrl: String,
-  },
-  {
-    timestamps: true,
-  }
-);
+    {
+        timestamps: true,
+    }
+)
 
-export default mongoose.model("Post", PostSchema);
+export default mongoose.model('Post', PostSchema)
